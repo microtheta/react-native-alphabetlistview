cccd7c90db4105c24c3c69dea671b1c5abc7c38d
Mahesh Thumar <thumarmahesh@gmail.com>
2017-12-01 16:19:15 +0530

    Update SectionList.js

--- components/SectionList.js
+++ components/SectionList.js
@@ -57,6 +57,8 @@ export default class SectionList extends Component {
     const targetY = ev.pageY;
     const { y, width, height } = this.measure;
     const index = (Math.floor(ev.locationY / height));
+    console.log(ev.locationY)
+    console.log(height)
     console.log(index)
     if (index >= this.props.sections.length) {
       return;
